531dd0459aef6ee1dc6b33170dd1830b0c04e485
iamchrispham <chrismpham@gmail.com>
2019-05-29 13:08:30 -0700

    Add files via upload
    completed queue/stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,13 +7,36 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+      storage[value] = value;
   };
 
   someInstance.dequeue = function() {
+    if (Object.keys(storage).length > 0) {
+      var temp = storage[Object.keys(storage)[0]];
+      console.log('Dequeue', temp);
+      delete storage[Object.keys(storage)[0]];
+	  return temp;
+    }
   };
 
   someInstance.size = function() {
+	  return Object.keys(storage).length;
   };
 
+  someInstance.front = function() {
+    if (Object.keys(storage).length >= 0){
+      return storage[Object.keys(storage)[0]];
+    } else {
+    return "Empty queue"
+    }
+  }
+
+  someInstance.back = function () {
+    if (Object.keys(storage).length >= 0) {
+      return storage[Object.keys(storage).length];
+    } else {
+      return "Empty queue"
+    }
+  }
   return someInstance;
 };
